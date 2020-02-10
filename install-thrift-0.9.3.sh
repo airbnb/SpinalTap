612d70e9dba07868c5ac6614aab7ca9fc878a400
Zuofei Wang <zuofei.wang@airbnb.com>
2020-02-10 11:50:59 -0800

    Patch install-thrift script

--- install-thrift-0.9.3.sh
+++ install-thrift-0.9.3.sh
@@ -3,7 +3,7 @@ set -ex
 wget http://archive.apache.org/dist/thrift/0.9.3/thrift-0.9.3.tar.gz
 tar xzf thrift-0.9.3.tar.gz
 cd thrift-0.9.3
-sed -i -e 's#mvn.repo=http://repo1.maven.org/maven2#mvn.repo=https://repo1.maven.org/maven2#g' ./lib/java/build.properties ./lib/java/build.properties-e
 ./configure --without-qt4 --without-qt5 --without-csharp --without-erlang --without-nodejs --without-lua --without-python --without-perl --without-php --without-php_extension --without-dart --without-ruby --without-haskell --without-go --without-haxe --without-d
+sed -i -e 's#mvn.repo=http://repo1.maven.org/maven2#mvn.repo=https://repo1.maven.org/maven2#g' ./lib/java/build.properties
 make
 sudo make install
